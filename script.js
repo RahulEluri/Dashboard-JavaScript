01ba002e2b025a8294e214048e2b930b5cde13cf
erahul <erahul@ariqt.com>
2024-04-15 19:11:59 +0530

    Third Commit

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
+// buttons
 let resetBtn=document.querySelector("#reset-btn");
 let resetAll=document.querySelector("#Reset-All");
 let arrSubmitBtn1=document.querySelector("#array-button1");
 let arrSubmitBtn2=document.querySelector("#array-button2");
 let strSubmitBtn=document.querySelector("#string-button");
 let number_btn=document.querySelector("#numb");
-
+// container
 let msgContainer1 = document.querySelector(".container1");
 let msgContainer2 = document.querySelector(".container2");
 let msgContainer3 = document.querySelector(".container3");
 
-// let text = "";
-// // let length = text.length;
-// if(text.length===0){
-//     console.log("sorry")
-// }
-// else{
-//     console.log(text);
-// }
 
+//string
 let string1;
 let string2;
 
@@ -27,110 +21,102 @@ const pushString=()=>{
     if(text.length===0){
         strSubmitBtn.disabled=true;
         alert("Enter a String");
-        // console.log('sorry');
     }
     else{
         string1=text;
         document.getElementById("demo1").innerHTML=string1;
         document.getElementById("stringy").value="";
-    
         arrSubmitBtn1.disabled=true;
         arrSubmitBtn2.disabled=true;
         msgContainer1.classList.add("hide");
         msgContainer2.classList.remove("hide");
         msgContainer3.classList.add("hide");
-        // console.log("hi");
     }
-    // console.log(string1);
     strSubmitBtn.disabled=false;
 }
 
 // string methods
         //charAt method
             const charAt_fn=()=>{
-                // console.log(string1)
+                
                 if(string1.length<=3){
-                    alert("Enter a String of minimum length of 4")
-                    // console.log("aythundi")
+                    alert("Enter a String of minimum length of 4");
                 }
                 else{
                     string2 = string1.charAt(3);
                     console.log(string2);
                     document.getElementById("demo3").innerHTML = string2;
-                    // console.log("layipoyindi")
                 }
-                // console.log("ayipoyindi")
             }
 
-        //split mehtod
+        //split method
             const split_fn=()=>{
                 if(string1.length<=3){
                     alert("Enter Strings with commas")
-                    // console.log("aythundi")
                 }
                 else{
                     string2 = string1.split(",");
-                    // console.log(string2);
-                    document.getElementById("demo3").innerHTML = "["+string2 +"] <br> <i>Type =></i>"+ typeof(string2) ;
-                    // console.log("layipoyindi")
+                    document.getElementById("demo3").innerHTML = `[${string2}] <br><br> <i>Type=> ${typeof(string2)}</i> ` ;
                 }
             }
 
         //lowercase method
             const lowercase_fn=()=>{
                 string2 = string1.toLowerCase();
-                // console.log(string2);
                 document.getElementById("demo3").innerHTML = string2;
             }
 
         //uppercase method
             const uppercase_fn=()=>{
                 string2 = string1.toUpperCase();
-                // console.log(string2);
                 document.getElementById("demo3").innerHTML = string2;
             }
 
         //indexOf method
             const indexOf_fn=()=>{
                 string2 = string1.indexOf("a");
-                // console.log(string2);
                 document.getElementById("demo3").innerHTML = string2+1;
             }
 
         //lastIndexOf method
             const lastIndexOf_fn=()=>{
                 string2 = string1.lastIndexOf("a");
-                // console.log(string2);
                 document.getElementById("demo3").innerHTML = string2+1;
             }
             
         //slice mehtod
         const slice_fn=()=>{
             if(string1.length<=5){
-                alert("Enter a String of minimum length of 5")
-                // console.log("aythundi")
+                alert("Enter a String of minimum length of 6")  
             }
             else{
                 string2 = string1.slice(2,5);
-                // console.log(string2);
-                document.getElementById("demo3").innerHTML = string2 +" <br> <i>Type =></i>"+ typeof(string2) ;
-                // console.log("layipoyindi")
+                document.getElementById("demo3").innerHTML = `${string2} <br><br> <i>Type=> ${typeof(string2)}</i> `;
             }
         }
-
-            
-
-
-
-
-
-
+        string2 +" <br> <i>Type =></i>"+ typeof(string2)
 
 
 //array
 let sampleArray1 = [];
 let sampleArray2 = [];
-let x = document.getElementById("numb").valueAsNumber;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSwitch = document.getElementById('flexSwitchCheckDefault');
+    const inputField = document.getElementById('numb');
+
+    toggleSwitch.addEventListener('change', function() {
+      if (this.checked) {
+        // Change input type to number when switch is checked
+        inputField.type = 'number';
+        //let x = document.getElementById("numb").valueAsNumber;
+    } else {
+        // Change input type to text when switch is unchecked
+        inputField.type = 'text';
+        //let x = document.getElementById("numb").value;
+      }
+    });
+  });
 
 
 // array1
@@ -140,11 +126,20 @@ function showArray1() {
 }
 
 function pushToArray1() {
-    let x = document.getElementById("numb").value;
-    // console.log('X ==>', x);
+    const toggleSwitch = document.getElementById('flexSwitchCheckDefault');
+    let x;
+    if (toggleSwitch.checked) {
+        x = document.getElementById("numb").valueAsNumber;
+    } else {
+        x = document.getElementById("numb").value;
+    }
 
-    if(x===""){
-        number_btn.disabled=true;
+    if (toggleSwitch.checked && isNaN(x)) {
+        alert("Enter a valid number");
+        number_btn.disabled = true;
+    } else if (!toggleSwitch.checked && (x === "" || x === null)) {
+        alert("Enter a valid value");
+        number_btn.disabled = true;
     }
     else{
         sampleArray1.push(x);
@@ -154,7 +149,7 @@ function pushToArray1() {
         msgContainer3.classList.add("hide");
     }
     number_btn.disabled=false;
-    // console.log('Array ==>',sampleArray1);
+    
     
      document.getElementById("numb").value="";
 
@@ -170,7 +165,8 @@ function showArray2() {
 
 function pushToArray2() {
     let x = document.getElementById("numb").value;
-    if(x===""){
+    if(x===""||x===null || isNaN(x)){
+        alert("Enter an Array Element")
         number_btn.disabled=true;
     }
     else{
@@ -181,7 +177,6 @@ function pushToArray2() {
         msgContainer3.classList.add("hide");
     }
     number_btn.disabled=false;
-    console.log('Array ==>',sampleArray2);
     
      document.getElementById("numb").value="";
 
@@ -193,13 +188,12 @@ function pushToArray2() {
             //map-method
                 const map_fn=()=>{
                     if(sampleArray1.length===0 && sampleArray2.length===0){
-                        alert("Enter an Array")
+                        alert("Enter an Array");
                     }
                     else{
                         let map1 = sampleArray1.map((x) => x * 2);
                         sampleArray1=map1;
                         document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
-                        // console.log(map1);
                     }
                 }
 
@@ -211,8 +205,7 @@ function pushToArray2() {
                     else{
                         let map1 = sampleArray1.filter((x) => x > 30);
                         sampleArray1=map1;
-                        document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
-                        // console.log(map1);
+                        document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";  
                     }
                 }
 
@@ -225,17 +218,54 @@ function pushToArray2() {
                         let map1 = sampleArray1.concat(sampleArray2);
                         sampleArray1=map1;
                         document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
-                        // console.log(map1);
                     }
                 }
 
+            //sort-method
+            const sort_fn=()=>{
+                
+                if(sampleArray1.length===0 && sampleArray2.length===0){
+                    alert("Enter an Array")
+                }
+                else if(typeof(sampleArray1[0])==="string"){
+                    let map2 = sampleArray1.sort();
+                    sampleArray1=map2;
+                    document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
+                }
+                else{
+                    let map1 = sampleArray1.sort(function(a, b){return a - b});
+                    sampleArray1=map1;
+                    document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
+                }
+            }
 
-//end-array methods
+            //sort-reverese-method
+            const sortrev_fn=()=>{
+                
+                if(sampleArray1.length===0 && sampleArray2.length===0){
+                    alert("Enter an Array")
+                }
+                else if(typeof(sampleArray1[0])==="string"){
+                    let map2 = sampleArray1.reverse();
+                    sampleArray1=map2;
+                    document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
+                }
+                else{
+                    let map1 = sampleArray1.sort(function(a, b){return b - a});
+                    sampleArray1=map1;
+                    document.getElementById("demo3").innerHTML = "["+sampleArray1+"]";
+                }
+            }
+
+// let poliyade=3;
+// console.log(typeof(poliyade));
+
+//end-array method
 
 // date
 const resetDate= ()=>{
     var today=new Date();  
-    document.getElementById('txt').innerHTML=today;
+    document.getElementById('txt').innerHTML=`<i>${today}</i>`;
 };
 //end date function
 
@@ -245,6 +275,7 @@ const reset=()=>{
     document.getElementById("demo1").innerHTML = "";
     document.getElementById("demo2").innerHTML = "";
     document.getElementById("demo3").innerHTML = "";
+    document.getElementById('txt').innerHTML   = "";
 
     strSubmitBtn .disabled=false;
     arrSubmitBtn1.disabled=false;
@@ -255,16 +286,16 @@ const reset=()=>{
     string1="";
     string2="";
 
-    msgContainer1.classList.add("hide");//array
-    msgContainer2.classList.add("hide");//string
-    msgContainer3.classList.remove("hide");//initial
+
+    msgContainer1.classList.add("hide");    //array
+    msgContainer2.classList.add("hide");    //string
+    msgContainer3.classList.remove("hide"); //initial
 }
 
 
 
 //event listener
 resetBtn.addEventListener("click",resetDate);
-
 resetAll.addEventListener("click",reset);
 
 
